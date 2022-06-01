26477e9834fc6b2030f17182331855c753300357
yangliu <15879180067@sina.cn>
2022-06-01 14:39:05 +0800

    feat: 部分功能 iOS 13 不支持.

--- Sources/Log/HTTP/HTTPSocket.swift
+++ Sources/Log/HTTP/HTTPSocket.swift
@@ -16,8 +16,7 @@ extension Net.HTTP {
 
         private let listener: NWListener
         private let queue = DispatchQueue(label: "Log.Net.HTTP.Socket", qos: .default)
-        @available(iOS 13.0, *)
-        private let context = NWConnection.ContentContext(identifier: "Log.Net.HTTP.Socket", metadata: [NWProtocolWebSocket.Metadata(opcode: .text)])
+        private let context: NWConnection.ContentContext
 
         deinit {
             listener.cancel()
@@ -29,7 +28,8 @@ extension Net.HTTP {
             }
             
             guard let port = NWEndpoint.Port(port) else { throw Net.Error.port }
-
+            context = .init(identifier: queue.label, metadata: [NWProtocolWebSocket.Metadata(opcode: .text)])
+            
             let parameters = NWParameters.tcp
             let options = NWProtocolWebSocket.Options()
             options.autoReplyPing = true
